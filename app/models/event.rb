7d4f67568f4b314913e21929e4d3ec7d8ddc43cc
Caleb Short <calebshort4697@gmail.com>
2021-02-01 20:30:51 -0500

    replaced model methods with scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,14 @@
 class Event < ApplicationRecord
   belongs_to :host, class_name: "User", foreign_key: "host_id"
   has_and_belongs_to_many :attendees, class_name: "User", foreign_key: "event_id", association_foreign_key: "attendee_id", join_table: "attendees_events"
+  scope :past, -> {where("date < ?", Date.today)}
+  scope :upcoming, -> {where("date >= ?", Date.today)}
 
-  def self.past
-    Event.all.where("date < ?", Date.today)
-  end
+  # def self.past
+  #   Event.all.where("date < ?", Date.today)
+  # end
 
-  def self.upcoming
-    Event.all.where("date >= ?", Date.today)
-  end
+  # def self.upcoming
+  #   Event.all.where("date >= ?", Date.today)
+  # end
 end
